a7e36cc57785c7c8d0b14b46f25171e30d91a28f
lin1heart <lin1heart@gmail.com>
2019-04-25 00:46:42 +0800

    random

--- js/index.js
+++ js/index.js
@@ -11,43 +11,65 @@ if (drawing.getContext){  //获取绘图上下文
     var drawWidth = cwidth/2;
     var drawHeight = cheight/2;
     content.translate(drawWidth,drawHeight);  //设置绘图原点
-    function heart(){
-    	
-        if(radian <= (Math.PI*2)){  //每增加一次弧度，绘制一条线
-            radian += radian_add;
-            X = getX(radian);
-            Y = getY(radian);
-            //求边缘点 0,0到x,y
-            if (X != 0 && Y < 0 && (Y/X < -1||Y/X > 1)){//y = -drawHeight            	
-            	A = -drawHeight*X/Y;
-            	B = -drawHeight;
-            }else if (Y != 0 && X > 0 && (Y/X > -1&&Y/X < 1)){// x=drawWidth
-            	A = drawWidth;
-            	B = Y/X*drawWidth;
-            }else if (X != 0 && Y > 0 && (Y/X < -1 || Y/X > 1)){//y=drawHeight
-            	A = drawHeight*X/Y;
-            	B = drawHeight;
-            }else {//x=-drawWidth
-            	A = -drawWidth;
-            	B = -drawWidth*Y/X;
-            }
-            content.moveTo(A,B);
-            content.lineTo(X,Y);
-            var grd = content.createLinearGradient(A,B,X,Y)//渐变起始和结束坐标
-　　　　		grd.addColorStop(0,"green");//渐变颜色
-			grd.addColorStop(0.3,"yellow");
-			grd.addColorStop(0.6,"red")
- 			grd.addColorStop(1,"blue");
-			content.lineWidth=3;
-            content.strokeStyle = grd;
-            content.stroke();
-            content.save();
-            content.beginPath();  //开始绘图
-        }
-    }
-    intervalId = setInterval(heart,100); 
+	var list1 = [];
+	var list2 = [];
+	while(radian <= (Math.PI*2)){  //每增加一次弧度，绘制一条线
+		radian += radian_add;
+		X = getX(radian);
+		Y = getY(radian);
+		list1.push({x:X,y:Y});
+		//求边缘点 0,0到x,y 边缘矩形4条边
+		if (X != 0 && Y < 0 && (Y/X < -1||Y/X > 1)){//y = -drawHeight            	
+			A = -drawHeight*X/Y;
+			B = -drawHeight;
+		}else if (Y != 0 && X > 0 && (Y/X > -1&&Y/X < 1)){// x=drawWidth
+			A = drawWidth;
+			B = Y/X*drawWidth;
+		}else if (X != 0 && Y > 0 && (Y/X < -1 || Y/X > 1)){//y=drawHeight
+			A = drawHeight*X/Y;
+			B = drawHeight;
+		}else {//x=-drawWidth
+			A = -drawWidth;
+			B = -drawWidth*Y/X;
+		}
+		list2.push({a:A,b:B});
+	}
+	draw();
 }
-
+var interVal;
+var time = 0;
+function draw(){
+	var size = list1.length;
+	time ++;
+	if(size > 0){
+		// content.save();
+		var j = Math.floor(Math.random()*size);
+		content.beginPath();
+		content.moveTo(list2[j].a,list2[j].b);
+		content.lineTo(list1[j].x,list1[j].y);
+		var grd = content.createLinearGradient(list2[j].a,list2[j].b,list1[j].x,list1[j].y)//渐变起始和结束坐标
+	　　 grd.addColorStop(0,"green");//渐变颜色
+		grd.addColorStop(0.3,"yellow");
+		grd.addColorStop(0.6,"red")
+		grd.addColorStop(1,"blue");
+		content.lineWidth=3;
+		content.strokeStyle = grd;
+		content.stroke();
+		list1.splice(j,1)
+		list2.splice(j,1)
+		size--;
+		// content.restore();
+		if (time < 5){
+			draw();
+		}else{
+			time = 0;
+			interVal = window.requestAnimationFrame(draw);
+		}
+		// setTimeout(function(){draw(content,list1,list2);},ti);
+	}else {
+		window.cancelAnimationFrame(interVal);
+	}
+}
 
 function getX(t){  //获取心型线的X坐标
     return 9*(16*Math.pow(Math.sin(t),3))
